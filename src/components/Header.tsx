282419975cd0d6bd9edc0981cc9db74f3174bd81
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-03-02 08:26:30 -0800

    Added styling to the app bar from boiler plate MUI app bar.
    - Changed the color to a grey color which matches the website background.
    - Changed title in app bar.
    - Changed/added button for projects page and resume download (Currently not functional).

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,26 +7,28 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Header() {
-  return (
+
+const sxStyle = {
+    width: "100%",
+    backgroundColor: "#3f3f3f"
+}
+
+
+export function Header() {
+
+    return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+        <AppBar position="sticky" sx={sxStyle}>
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Rajvir's Portfolio
+                </Typography>
+                <Button variant="outlined" color="inherit" sx={{mr: 2}}>
+                    Projects
+                    </Button>
+                <Button variant="outlined" color="inherit">Resume</Button>
+            </Toolbar>
+        </AppBar>
     </Box>
-  );
+    );
 }
