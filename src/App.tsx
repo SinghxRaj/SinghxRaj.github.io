b028d233664ca6bf8b0d16f5247bc6d950d74e5b
RickySin11 <rajvir11singh@outlook.com>
2023-03-27 06:45:19 -0700

    Add Hero, Intro, Footer components to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
-import { Header } from './components/Header'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Intro from './components/Intro'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Hero />
+      <Intro />
+      <Footer />
     </div>
   )
 }
