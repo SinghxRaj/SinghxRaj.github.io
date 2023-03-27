82f2780a10c51c1cf3eb3ed65cf8973b66cc701c
RickySin11 <rajvir11singh@outlook.com>
2023-03-27 06:39:47 -0700

    Change the position of the app bar from sticky to relative

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,31 @@
-
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
 
 const sxStyle = {
     width: "100%",
-    backgroundColor: "#3f3f3f"
+    backgroundColor: "#3f3f3f",
+    position: "relative"
 }
 
 
-export function Header() {
+export default function Header() {
 
     return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="sticky" sx={sxStyle}>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Rajvir's Portfolio
-                </Typography>
-                <Button variant="outlined" color="inherit" sx={{mr: 2}}>
-                    Projects
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar sx={sxStyle}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Rajvir's Portfolio
+                    </Typography>
+                    <Button variant="outlined" color="inherit" sx={{ mr: 2 }}>
+                        Projects
                     </Button>
-                <Button variant="outlined" color="inherit">Resume</Button>
-            </Toolbar>
-        </AppBar>
-    </Box>
+                    <Button variant="outlined" color="inherit">Resume</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
