d1cc5e427228d8341fe67b0cc24f9122fa3b8450
RickySin11 <rajvir11singh@outlook.com>
2023-07-11 15:10:46 -0700

    Remove react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
       <Footer />
     </div>
   )
